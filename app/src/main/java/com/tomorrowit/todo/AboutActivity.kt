b0476dd6ccef695242853d3de64bb66a7b726799
Alexandru Cehan <cehanalexandru@gmail.com>
2022-03-28 17:42:26 +0300

    Added AboutActivity.
    Added about activity.
    Added a local HTML file for it's webview.

---
+++ app/src/main/java/com/tomorrowit/todo/AboutActivity.kt
@@ -0,0 +1,18 @@
+package com.tomorrowit.todo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tomorrowit.todo.databinding.ActivityAboutBinding
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityAboutBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.toolbar.title = getString(R.string.app_name)
+        binding.about.loadUrl("file:///android_asset/about.html")
+    }
+}
--- app/src/main/java/com/tomorrowit/todo/MainActivity.kt
+++ app/src/main/java/com/tomorrowit/todo/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.tomorrowit.todo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.tomorrowit.todo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,14 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.about -> {
+            startActivity(Intent(this, AboutActivity::class.java))
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
