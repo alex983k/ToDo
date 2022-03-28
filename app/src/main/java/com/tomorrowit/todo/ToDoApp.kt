c50b1442af3250f88cc322531a74bcac8e5b3263
Alexandru Cehan <cehanalexandru@gmail.com>
2022-03-28 18:26:35 +0300

    Created a viewmodel.
    Created RosterMotor as a ViewModel.
    Passed it to Koin in the Application subclass.
    Created a new parameter of ViewModel inside RosterListFragment that takes a viewmodel from Koin.

--- app/src/main/java/com/tomorrowit/todo/ToDoApp.kt
+++ app/src/main/java/com/tomorrowit/todo/ToDoApp.kt
@@ -2,6 +2,7 @@ package com.tomorrowit.todo
 
 import android.app.Application
 import org.koin.android.ext.koin.androidLogger
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
@@ -9,11 +10,13 @@ class ToDoApp : Application() {
 
     /**
      * [single] defines an object that will be available as a Koin-managed singleton.
+     * [viewModel] defines an object that uses AndroidX ViewModel system to make it available to those activities and fragments that need it.
      * [module] is part of a Koin domain-specific language (DSL) that describes the roster of objects to be available via dependency inversion.
      * You can of course have one or several Koin modules.
      */
     private val koinModule = module {
         single { ToDoRepository() }
+        viewModel { RosterMotor(get()) }
     }
 
     override fun onCreate() {
