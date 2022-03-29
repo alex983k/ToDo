ae4984e4720d0266433a5056e3af6c78d9cfbce2
Alexandru Cehan <cehanalexandru@gmail.com>
2022-03-29 17:12:46 +0300

    Added logic for add item.
    Added logic for add item.
    Added + button for RosterListFragment,
    Removed the basic list with an empty one for the moment.

--- app/src/main/java/com/tomorrowit/todo/RosterListFragment.kt
+++ app/src/main/java/com/tomorrowit/todo/RosterListFragment.kt
@@ -1,9 +1,7 @@
 package com.tomorrowit.todo
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -16,6 +14,11 @@ class RosterListFragment : Fragment() {
     private var binding: TodoRosterBinding? = null
 
     private val motor: RosterMotor by viewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,16 +36,16 @@ class RosterListFragment : Fragment() {
         expression outside of the function call parentheses. So, the lambda expression that
         we have here turns into onCheckboxToggle.*/
         //val adapter = RosterAdapter(layoutInflater) {
-            /*Here, we create the updated model by using copy(), a function added to all Kotlin
-            data classes. As the name suggests, copy() makes a copy of the immutable object,
-            except it replaces whatever properties we include as parameters to the copy() call. In
-            our case, we replace isCompleted with the opposite of its current value.*/
-            //motor.save(it.copy(isCompleted = !it.isCompleted))
+        /*Here, we create the updated model by using copy(), a function added to all Kotlin
+        data classes. As the name suggests, copy() makes a copy of the immutable object,
+        except it replaces whatever properties we include as parameters to the copy() call. In
+        our case, we replace isCompleted with the opposite of its current value.*/
+        //motor.save(it.copy(isCompleted = !it.isCompleted))
         //}
 
         val adapter = RosterAdapter(
             layoutInflater,
-            onCheckboxToggle = {motor.save(it.copy(isCompleted = !it.isCompleted))},
+            onCheckboxToggle = { motor.save(it.copy(isCompleted = !it.isCompleted)) },
             onRowClick = ::display
         )
 
@@ -59,7 +62,25 @@ class RosterListFragment : Fragment() {
         }
 
         adapter.submitList(motor.getItems())
-        binding?.empty?.visibility = View.GONE
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.actions_roster, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add -> {
+                add()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun add() {
+        findNavController().navigate(RosterListFragmentDirections.createModel(null))
     }
 
     private fun display(model: ToDoModel) {
--- app/src/main/java/com/tomorrowit/todo/ToDoRepository.kt
+++ app/src/main/java/com/tomorrowit/todo/ToDoRepository.kt
@@ -1,20 +1,7 @@
 package com.tomorrowit.todo
 
 class ToDoRepository {
-    var items = listOf(
-        ToDoModel(
-            description = "Buy food from _Cool Food_",
-            isCompleted = true,
-            notes = "Check their website for discounts"
-        ),
-        ToDoModel(
-            description = "Buy milk"
-        ),
-        ToDoModel(
-            description = "Buy some eggs",
-            notes = "Make sure to buy 10!"
-        )
-    )
+    var items = emptyList<ToDoModel>()
 
     fun save(model: ToDoModel) {
         items = if (items.any { it.id == model.id }) {
@@ -24,5 +11,10 @@ class ToDoRepository {
         }
     }
 
-    fun find(modelId: String) = items.find { it.id == modelId }
+    fun delete(model: ToDoModel) {
+        //Here we just replace items with a filtered edition of items, keeping any item that has an ID different than the one that we are trying to remove.
+        items = items.filter { it.id != model.id }
+    }
+
+    fun find(modelId: String?) = items.find { it.id == modelId }
 }
